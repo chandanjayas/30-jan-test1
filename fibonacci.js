3aaca03f2bef1d17c0d3e2c8074f06672e19818c
chandan jayas <90899405+chandanjayas@users.noreply.github.com>
2022-01-30 14:04:07 +0530

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+// write a program to print the nth fibonacci number .take user input.
+
+
+var num=prompt("Enter a number");
+var num1=parseInt(num);
+var a=0;
+var b=1;
+var sum=a+b;
+console.log(a);
+console.log(b);
+for(var i=1; i<num1; i++){
+    console.log(sum)
+    a=b;
+    b=sum;
+    sum=a+b; 
+    
+}
